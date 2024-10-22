072c846108c62b2347c0f71a7540e01bee046a9b
Michal Stehlík <michal.stehlik@pslib.cz>
2024-10-22 12:13:29 +0200

    Add project files.

---
+++ C01ParallelSort/Program.cs
@@ -0,0 +1,21 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Vytvoření nového vlákna
+Thread thread = new Thread(new ThreadStart(DoWork));
+
+// Spuštění vlákna
+thread.Start();
+
+// Pokračování hlavního vlákna
+for (int i = 0; i < 5; i++)
+{
+    Console.WriteLine("Hlavní vlákno: " + i);
+    Thread.Sleep(1000); // Simulace práce ve vlákně
+}
+static void DoWork()
+{
+    for (int i = 0; i < 5; i++)
+    {
+        Console.WriteLine("Vedlejší vlákno: " + i);
+        Thread.Sleep(1000); // Simulacetoho, že operace chvíli trvá
+    }
+}
